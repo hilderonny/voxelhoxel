0d99128cc8009c2f33cdb16f1714b24d9d790bc5
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-12-10 22:15:00 +0100

    Added registration

--- create/create.js
+++ create/create.js
@@ -3,7 +3,7 @@
 const palette64 = ['#000000', '#00177D', '#024ACA', '#0084FF', '#5BA8FF', '#98DCFF', '#9BA0EF', '#6264DC', '#3D34A5', '#211640', '#5A1991', '#6A31CA', '#A675FE', '#E2C9FF', '#FEC9ED', '#D59CFC', '#CC69E4', '#A328B3', '#871646', '#CF3C71', '#FF82CE', '#FFE9C5', '#F5B784', '#E18289', '#DA655E', '#823C3D', '#4F1507', '#E03C28', '#E2D7B5', '#C59782', '#AE6C37', '#5C3C0D', '#231712', '#AD4E1A', '#F68F37', '#FFE737', '#FFBB31', '#CC8F15', '#939717', '#B6C121', '#EEFFA9', '#BEEB71', '#8CD612', '#6AB417', '#376D03', '#172808', '#004E00', '#139D08', '#58D332', '#20B562', '#00604B', '#005280', '#0A98AC', '#25E2CD', '#BDFFCA', '#71A6A1', '#415D66', '#0D2030', '#151515', '#343434', '#7B7B7B', '#A8A8A8', '#D7D7D7', '#FFFFFF'];
 const dbName = 'voxelhoxel', collectionName = 'models';
 
-var arrangeClient, listLoginForm, list, listToolbar, rendering, toolbar, colorpalette, colorbar, user, _id, model;
+var arrangeClient, listLoginForm, listRegisterForm, list, listToolbar, rendering, toolbar, colorpalette, colorbar, user, _id, model;
 
 
 /******* LISTE **********/
@@ -18,6 +18,27 @@ async function login() {
         document.getElementById('loginfailed').style.display = 'initial';
     } else {
         user = loginResult;
+        showList();
+    }
+}
+
+async function register() {
+    const rf1 = document.getElementById('registrationfailed1');
+    const rf2 = document.getElementById('registrationfailed2');
+    rf1.style.display = 'none';
+    rf2.style.display = 'none';
+    const username = listRegisterForm.querySelector('[name="username"]').value;
+    const password1 = listRegisterForm.querySelector('[name="password1"]').value;
+    const password2 = listRegisterForm.querySelector('[name="password2"]').value;
+    if (!password1 || !password2 || password1 !== password2) {
+        rf2.style.display = 'initial';
+        return;
+    }
+    const registerResult = await arrangeClient.register('voxelhoxel', username, password1);
+    if (!registerResult._id) {
+        rf1.style.display = 'initial';
+    } else {
+        user = registerResult;
         showList();
     }
 }
@@ -49,12 +70,18 @@ async function showList() {
         list.appendChild(el);
     });
     listLoginForm.style.display = 'none';
+    listRegisterForm.style.display = 'none';
     list.style.display = 'block';
     listToolbar.style.display = 'flex';
     rendering.style.display = 'none';
     toolbar.style.display = 'none';
     colorpalette.classList.remove('visible');
     colorbar.style.display = 'none';
+}
+
+function showRegistrationForm() {
+    listLoginForm.style.display = 'none';
+    listRegisterForm.style.display = 'flex';
 }
 
 /******* EDITOR **********/
@@ -77,6 +104,7 @@ async function showModel(id) {
     };
 
     listLoginForm.style.display = 'none';
+    listRegisterForm.style.display = 'none';
     list.style.display = 'none';
     listToolbar.style.display = 'none';
     rendering.style.display = 'flex';
@@ -186,6 +214,7 @@ window.addEventListener('load', async function () {
     arrangeClient = await ArrangeClient().connect('wss://arrange.avorium.de:42001');
 
     listLoginForm = document.getElementById('listloginform');
+    listRegisterForm = document.getElementById('listregisterform');
     list = document.getElementById('list');
     listToolbar = document.getElementById('listtoolbar');
     rendering = document.getElementById('rendering');
