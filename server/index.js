75fe701a6788a72f8fbfea36d6cb5cc23fa17793
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2019-01-03 19:55:31 +0100

    Added express server.

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../client'));
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+server.listen(port, function () {
+    console.log('Server running at port ' + port);
+});
