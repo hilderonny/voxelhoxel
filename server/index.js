0c318a3f8d6d5c7af0ccab48f5a2486312402439
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2019-01-09 14:00:03 +0100

    Added CORS to server to allow acces from native apps.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const api = require('./api');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const cors = require('cors');
 const express = require('express');
 const http = require('http');
 
 const app = express();
 app.use(compression());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client'));
