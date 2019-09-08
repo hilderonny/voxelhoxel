b38b60d842aa8f6b50b9f5174e9920bdbc33b4de
hilderonny <gitlab@hildebrandt2014.de>
2019-09-08 14:04:01 +0200

    Liste fertig gemacht.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,11 +3,63 @@ window.addEventListener('load', function () {
     // Lokale Modelle aus der IndexedDb laden und anzeigen
     LocalDb.listModels().then(function (localModels) {
         console.log(localModels);
-        // TODO: Anzeigen
-
+        // Erst mal alle lokalen Modelle anzeigen
+        localModels.forEach(function(localModel) {
+            addModelToList(localModel);
+        });
         // Anschließend Modelle vom Server laden und dabei den Ladespinner anzeigen
-        
-
+        showProgressBar();
+        var db = firebase.database();
+        // Erst mal die Metadaten aus der Datenbank holen. Id und Zeitpunkt der letzten Änderung
+        db.ref('/modelmetas/').once('value').then(async function (metasnapshot) {
+            // Diese Methode ist nach dem async/await Schema, weil wir nach den ganzen asynchronen Aufrufen
+            // aller Modelle den Ladespinner wieder ausmachen müssen.
+            var modelmetas = metasnapshot.val();
+            var storage = firebase.storage(); // Storage-API zum Laden der Vorschaubilder vom Server
+            var entries = Object.entries(modelmetas);
+            for (var i = 0; i < entries.length; i++) {
+                [id, modelmeta] = entries[i];
+                // Modelle, die noch nicht veröffentlicht sind, sollen auch nicht angezeigt werden
+                if (!modelmeta.published) continue;
+                // Prüfen, ob das Modell bereits lokal vorhanden ist
+                var localModelIndex = localModels.findIndex(function (m) { return m._id === id });
+                if (localModelIndex >= 0) {
+                    var localModel = localModels[localModelIndex];
+                    // Wenn das lokale Modell genauso alt ist oder bereits daran gemalt wurde, soll das lokale genommen werden
+                    if ((localModel.painted && localModel.painted.length > 0) || (localModel.lastmodified >= modelmeta.lastmodified)) {
+                        // When the local model is already painted, or local model is newer, ignore the server model
+                        continue;
+                    }
+                }
+                // Hier gibt es entweder kein lokales Modell, oder es wurde noch nicht bearbeitet und auf dem Server gibt es ein Update.
+                // In diesem Falle soll es erneut vom Server geladen werden.
+                var model = {
+                    _id: id, // Wird als Key in lokaler IndexedDB verwendet
+                    lastmodified: modelmeta.lastmodified
+                };
+                // Vorschaubild per Storage API laden
+                var url = await storage.ref('/modelthumbnails/' + id + '.jpg').getDownloadURL();
+                // Lokal wird das Vorschaubild als Base64 Zeichenkette in der IndexedDB gespeichert.
+                var base64 = await getBase64ImageFromUrl(url);
+                // Vorschaubild im lokalen Modell speichern.
+                model.thumbnail = base64;
+                // Details des Modells vom Server laden: Szene, Farbpalette, etc.
+                var detailssnapshot = await db.ref('/modeldetails/' + id).once('value');
+                var modeldetail = detailssnapshot.val();
+                // Relevante Daten vom Server im lokalen Modell speichern
+                model.scene = modeldetail.scene;
+                model.pos = modeldetail.pos;
+                model.colorpalette = modeldetail.colorpalette;
+                model.target = modeldetail.target;
+                // An dieser Stelle ist das Modell vom Server fertig geladen.
+                // Es ist noch nicht in der lokalen Datenbank, also speichern wir es dort rein.
+                await LocalDb.saveModel(model);
+                localModels.push(model);
+                // Now model is ready to be shown in list
+                addModelToList(model);
+            }
+            hideProgressBar();
+        });
     });
 
 });
---
+++ public/scripts/utils.js
@@ -0,0 +1,37 @@
+function showProgressBar() {
+    document.querySelector('.progressbar').classList.remove('invisible');
+}
+
+function hideProgressBar() {
+    document.querySelector('.progressbar').classList.add('invisible');
+}
+
+// Fügt das Bild eines Modells in die Liste ein und verlinkt es mit der Detailansicht.
+function addModelToList(model) {
+    var list = document.querySelector('#listpage .grid');
+    var el = document.createElement('li');
+    el.innerHTML = '<img src="' + model.thumbnail + '" class="' + ((!model.painted || Object.keys(model.painted).length < 1) ? 'new' : '') + (model.complete ? ' complete' : '') + '"/><span class="new">Neu</span><span class="complete">&#10004;</span>';
+    el.addEventListener('click', function () {
+        showPlayModel(model);
+    });
+    // Beim Speichern wird eine Referenz auf das HTML Element benötigt, damit das Vorschaubild und der Status aktualisiert werden können.
+    model.listEl = el;
+    list.appendChild(el);
+}
+
+// Von https://staxmanade.com/2017/02/how-to-download-and-convert-an-image-to-base64-data-url/
+// Blob in Base64 Zeichenkette umwandelt
+async function getBase64ImageFromUrl(imageUrl) {
+    var res = await fetch(imageUrl);
+    var blob = await res.blob();
+    return new Promise((resolve, reject) => {
+        var reader = new FileReader();
+        reader.addEventListener("load", function () {
+            resolve(reader.result);
+        }, false);
+        reader.onerror = () => {
+            return reject(this);
+        };
+        reader.readAsDataURL(blob);
+    })
+}
