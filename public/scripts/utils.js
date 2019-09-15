ccaa5b7c38153a320c5da5219ca0caf9a3c2aec1
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2019-09-15 21:26:55 +0200

    PWA- und Offline-fähig gemacht.

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,133 +1,49 @@
-// Fügt das Bild eines Modells in die Liste ein und verlinkt es mit der Detailansicht.
-function addModelToList(model) {
-    var list = document.querySelector('#listpage .grid');
-    var el = document.createElement('li');
-    el.innerHTML = '<img src="' + model.thumbnail + '" class="' + ((!model.painted || Object.keys(model.painted).length < 1) ? 'new' : '') + (model.complete ? ' complete' : '') + '"/><span class="new">Neu</span><span class="complete">&#10004;</span>';
-    el.addEventListener('click', function () {
-        el.classList.add('progressspinner');
-        showPlayModel(model);
-        el.classList.remove('progressspinner');
+// Dieses Script sollte mit "defer" eingebunden werden, damit das Seitenladen schnell geht.
+// Am Ende wird ein Service worker für die Progressive Web App (PWA) eingebunden
+
+/**
+ * Sammlung von Hilfsfunktionen
+ */
+var UTILS = {
+
+    /* Ein- und Ausblenden von Elementen. Das wird über die CSS Klasse "invisible" gesteuert. */
+
+    /**
+     * Element sichtbar machen. Dazu wird die CSS Klasse "invisible" vom Element entfernt.
+     * @param {*} selector Normalerweise eine ID in der Form "#ElementId". Kann aber auch ein CSS Selektor sein.
+     */
+    showElement: function(selector) {
+        var element = document.querySelector(selector);
+        if (element) element.classList.remove('invisible');
+    },
+
+    /**
+     * Element unsichtbar machen. Dazu wird die CSS Klasse "invisible" dem Element hinzugefügt.
+     * @param {*} selector Normalerweise eine ID in der Form "#ElementId". Kann aber auch ein CSS Selektor sein.
+     */
+    hideElement: function(selector) {
+        var element = document.querySelector(selector);
+        if (element) element.classList.add('invisible');
+    },
+
+    /**
+     * Schaltet eine CSS Klasse auf einem Element um
+     * @param {*} selector CSS Selektor für das Element
+     * @param {*} cls Klasse, die aktiviert oder deaktiviert werden muss
+     */
+    toggleClass: function(selector, cls) {
+        document.querySelector(selector).classList.toggle(cls);
+    },
+
+};
+
+
+// Service worker einbinden. Dieser muss im Stammverzeichnis der App in der Datei "serviceworker.js"
+// enthalten sein.
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        var serviceWorkerFile = 'serviceworker.js';
+        console.log('%c🧰 load: Registriere service worker aus Datei ' + serviceWorkerFile, 'color:yellow');
+        navigator.serviceWorker.register(serviceWorkerFile);
     });
-    // Beim Speichern wird eine Referenz auf das HTML Element benötigt, damit das Vorschaubild und der Status aktualisiert werden können.
-    model.listEl = el;
-    list.appendChild(el);
-}
-
-// Von https://staxmanade.com/2017/02/how-to-download-and-convert-an-image-to-base64-data-url/
-// Blob in Base64 Zeichenkette umwandelt
-async function getBase64ImageFromUrl(imageUrl) {
-    var res = await fetch(imageUrl);
-    var blob = await res.blob();
-    return new Promise((resolve, reject) => {
-        var reader = new FileReader();
-        reader.addEventListener("load", function () {
-            resolve(reader.result);
-        }, false);
-        reader.onerror = () => {
-            return reject(this);
-        };
-        reader.readAsDataURL(blob);
-    })
-}
-
-// Lädt ein Modell in den Spielemodus und zeigt die Spielseite an
-// Wird asynchron ausgeführt, weil das Laden eine Weile dauern kann
-function showPlayModel(model) {
-    Player.loadModel(model);
-    showPage('playpage');
-}
-
-// Wenn auf den Backbutton gedrückt wurde, woll das Modell lokal gespeichert und danach die Liste angezeigt werden
-function goBack() {
-    hidePage('playpage');
-}
-
-function resetModel() {
-    console.log('CLEAR');
-}
-
-// Erzeugt einen Würfel an einer bestimmten Position mittels einzelner Flächen
-// Dabei wird angegeben, ob bestimmte Flächen erszeugt werden sollen, oder nicht
-// Wird für Play Modus verwendet
-function createPlayBox(standardMaterial, x, y, z, top, bottom, left, right, front, back) {
-    var boxMesh = new THREE.Group();
-    // Nur die Flächen erzeugen, die auch wirklich sichtbar sind
-    if (top) boxMesh.add(createPlane(standardMaterial, [0, .5, 0], [-1.5708, 0, 0]));
-    if (bottom) boxMesh.add(createPlane(standardMaterial, [0, -.5, 0], [1.5708, 0, 0]));
-    if (left) boxMesh.add(createPlane(standardMaterial, [-.5, 0, 0], [0, -1.5708, 0]));
-    if (right) boxMesh.add(createPlane(standardMaterial, [.5, 0, 0], [0, 1.5708, 0]));
-    if (front) boxMesh.add(createPlane(standardMaterial, [0, 0, .5], [0, 0, 0]));
-    if (back) boxMesh.add(createPlane(standardMaterial, [0, 0, -.5], [0, 3.14159, 0]));
-    // Würfel positionieren
-    boxMesh.position.x = x;
-    boxMesh.position.y = y;
-    boxMesh.position.z = z;
-    boxMesh.updateMatrix();
-    boxMesh.matrixAutoUpdate = false;
-    return boxMesh;
-}
-
-// Erzeugt eine einzelne Fläche für einen Würfel
-function createPlane(material, position, rotation) {
-    var geometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1);
-    var planeMesh = new THREE.Mesh(geometry, material);
-    planeMesh.position.x = position[0];
-    planeMesh.position.y = position[1];
-    planeMesh.position.z = position[2];
-    if (rotation[0]) planeMesh.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), rotation[0]);
-    if (rotation[1]) planeMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), rotation[1]);
-    if (rotation[2]) planeMesh.setRotationFromAxisAngle(new THREE.Vector3(0, 0, 1), rotation[2]);
-    planeMesh.updateMatrix();
-    planeMesh.matrixAutoUpdate = false;
-    return planeMesh;
-}
-
-// Erzeugt ein Material mit einer Farbe (wenn als Hex gegeben) oder einer Bildtexture (wenn URL angegeben)
-function createStandardMaterial(hexColorOrUrl) {
-    if (hexColorOrUrl.length > 7) {
-        // Parameter enthält URL für Textur, z.B. https://i.imgur.com/iy50ZFn.png
-        // Meine Bilder liegen bei https://hilderonny.imgur.com/all/?third_party=1
-        var texture = new THREE.TextureLoader().load(hexColorOrUrl);
-        texture.magFilter = THREE.NearestFilter;
-        texture.minFilter = THREE.LinearMipMapLinearFilter;
-        return new THREE.MeshLambertMaterial( { map: texture } );
-    } else {
-        return new THREE.MeshLambertMaterial( { color: hexColorOrUrl } );
-    }
-}
-
-// Erzeugt ein Material mit einer Textur, die einen Text enthält
-function createNumberMaterial(number) {
-    const bitmap = document.createElement('canvas');
-    bitmap.width = 128;
-    bitmap.height = 128;
-    const context = bitmap.getContext('2d');
-    context.fillStyle = '#000000';
-    context.fillRect(0, 0, 128, 128);
-    context.fillStyle = '#eeeeee';
-    context.fillRect(1, 1, 126, 126);
-    context.font = 'Bold 64px sans-serif';
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    context.fillStyle = '#444444';
-    context.fillText(number, 64, 64);
-    const texture = new THREE.Texture(bitmap);
-    texture.needsUpdate = true;
-    return new THREE.MeshLambertMaterial({ map: texture });
-}
-
-function showPage(pageId) {
-    document.getElementById(pageId).classList.remove('invisible');
-}
-
-function hidePage(pageId) {
-    document.getElementById(pageId).classList.add('invisible');
-}
-
-function showProgressBar() {
-    document.querySelector('.progressbar').classList.remove('invisible');
-}
-
-function hideProgressBar() {
-    document.querySelector('.progressbar').classList.add('invisible');
 }
---
+++ public/serviceworker.js
@@ -0,0 +1,61 @@
+// Per default stsuert ein Service worker alle Anfragen, die in seinem und aller Unterverzeichnisse
+// ankommen. Daher ist es am einfachsten, den worker in das Stammverzeichnis der App zu legen, dann
+// braucht man nämlich nicht den Scope anzugeben.
+
+// Der Service Worker wird bei jedem Aufruf der Website geladen. Wenn der Browser mitkriegt, dass sich
+// diese Datei geändert hat, wird der Service Worker erneut installiert und 'activate' aufgerufen.
+
+// Dieser Name ist ein Hilfsmittel, das beim Löschen des alten und Neuaufbau des neuen Caches hilft.
+// Wenn dieser Name geändert wird und der Service worker neu installiert wird. führt das bei activate()
+// dazu, dass der alte Cache gelöscht und bei fetch() dazu, dass alle zu cachenden Dateien neu geladen werden.
+var CACHE_NAME = 'voxelhoxel-1';
+
+// Diese Funktion wird bei der Neuinstallation des Service workers aufgerufen.
+self.addEventListener('install', function(evt) {
+    console.log('%c⚙ install: Neuinstallation nach Änderung der Service worker Datei', 'color:lightgrey');
+    // skipWaiting dient dazu, dass die vorherige Version des workers beendet und diese neue
+    // Version gleich installiert und aktiviert wird, ohne zu warten
+    self.skipWaiting();
+});
+
+// Diese Funktion wird nach jeder Neuinstallation des Service workers ausgeführt und dient dazu,
+// alte Caches zu löschen
+self.addEventListener('activate', function (evt) {
+    console.log('%c⚙ activate: Service worker mit Cache Name "' + CACHE_NAME + '" ist nun aktiv.', 'color:lightgrey');
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('%c⚙ activate: Lösche alten Cache "' + key + '"', 'color:lightgrey');
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    // Bringt den Browser dazu, den neuen Service worker sofort zu benutzen und nicht erst beim nächsten Laden der Seite
+    self.clients.claim();
+});
+
+// Netzwerkabfragen abfangen und im Offline Betrieb aus Cache bereit stellen
+self.addEventListener('fetch', function (evt) {
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(async function (cache) {
+            try {
+                // Versuchen, die Datei aus dem Netz zu laden. 'reload' umgeht dabei den Browser-eigenen Cache, damit die Dateien
+                // zwangsweise neu geladen werden. Ist bei js-Dateien ganz hilfreich, weil der Browser diese sonst nicht neu lädt
+                var response = await fetch(evt.request, {cache: 'reload'});
+                // Wenn der Zugriff auf das Netz geklappt hat, die Datei im Cache speichern
+                if (response.status === 200) {
+                    console.log('%c⚙ fetch: Speichere im Cache: ' + evt.request.url, 'color:lightgrey');
+                    cache.put(evt.request.url, response.clone());
+                }
+                return response;
+            }
+            catch (err) {
+                // Netzwerkzugriff fehlgeschlagen, also aus dem Cache holen
+                console.log('%c⚙ fetch: Liefere aus dem Cache: ' + evt.request.url, 'color:lightgrey');
+                return cache.match(evt.request);
+            }
+        })
+    );
+});
